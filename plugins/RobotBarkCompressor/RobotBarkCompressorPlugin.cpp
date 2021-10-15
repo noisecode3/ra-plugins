6aaf9223bf9cd4516d5f8455bb258fcce767f99b
Martin Bångens <marbangens@gmail.com>
2021-10-15 18:47:01 +0200

    makefile reado for Include dir add fft.h

--- plugins/RobotBarkCompressor/RobotBarkCompressorPlugin.cpp
+++ plugins/RobotBarkCompressor/RobotBarkCompressorPlugin.cpp
@@ -32,6 +32,8 @@ RobotBarkCompressorPlugin::RobotBarkCompressorPlugin()
 {
     // set default values
     loadProgram(0);
+    //fft myfft;
+    //myfft.setup(512);
 }
 
 // -----------------------------------------------------------------------
